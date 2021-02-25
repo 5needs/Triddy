ccd434c502652cbeaf8a222e383221ebecc169ac
Ricar8o <andres.martinez-d@mail.escuelaing.edu.co>
2021-02-24 21:53:12 -0500

    Login basic form

---
+++ src/Components/login/Login.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import { Avatar, Box, Button, Checkbox, CssBaseline, FormControlLabel, Grid, Link, TextField, Typography } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import './Login.css';
+
+export class Login extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <Container className="container" component="main" maxWidth="xs">
+                <CssBaseline/>
+                <div className="paper" >
+                    <Avatar className="avatar">
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <form className="form" noValidate>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button 
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className="submit"
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container className="linksGrid">
+                            <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Forgot password?
+                            </Link>
+                            </Grid>
+                            <Grid item>
+                            <Link href="#" variant="body2">
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </div>
+            </Container>
+        );
+    }
+}
