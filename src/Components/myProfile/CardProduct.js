97d9efccaa09deee48ea1d6bf3678574b91b046d
David Andres Vargas Leon <47030575+GEARSHORDA3@users.noreply.github.com>
2021-03-01 13:31:30 -0500

    add prove

--- src/Components/myProfile/CardProduct.js
+++ src/Components/myProfile/CardProduct.js
@@ -7,30 +7,30 @@ import Button from '@material-ui/core/Button';
 class CardProduct extends React.Component {
 
     render(){
-        return (
-            <div className="card mx-auto Fitness-Card"
-                 style={{
-                     backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${leftColor}, ${rightColor})`
-                 }}
-            >
-                <div className="card-body">
-                    <div className="row center">
-                        {img}
-                        <div className="col-6 Fitness-Card-Info">
-                            <h1>{title}</h1>
-                            <p>Price ${price}</p>
-                            <Button variant="contained" color="primary">
-                                Editar
-                            </Button>
-                            <Button variant="contained" color="secondary">
-                                Eliminar
-                            </Button>
-                        </div>
+        // return (
+        //     <div className="card mx-auto Fitness-Card"
+        //          style={{
+        //              backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${leftColor}, ${rightColor})`
+        //          }}
+        //     >
+        //         <div className="card-body">
+        //             <div className="row center">
+        //                 {img}
+        //                 <div className="col-6 Fitness-Card-Info">
+        //                     <h1>{title}</h1>
+        //                     <p>Price ${price}</p>
+        //                     <Button variant="contained" color="primary">
+        //                         Editar
+        //                     </Button>
+        //                     <Button variant="contained" color="secondary">
+        //                         Eliminar
+        //                     </Button>
+        //                 </div>
 
-                    </div>
-                </div>
-            </div>
-        )
+        //             </div>
+        //         </div>
+        //     </div>
+        // )
     }
 }
 
