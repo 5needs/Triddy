814f5a081c3662d568347fcf8476ac1cb5dd1a73
Edyesid <54111499+Edyesid@users.noreply.github.com>
2021-03-01 23:32:05 -0500

    Add files via upload

--- src/Components/rent-product/AddressForm.js
+++ src/Components/rent-product/AddressForm.js
@@ -7,52 +7,55 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import MenuItem from '@material-ui/core/MenuItem';
 
-export default function AddressForm() {
+export class AddressForm extends React.Component {
+  render() {
   const numbers = [{value: "1"},{value: 2},{value: 3},{value: 4},{value: 5},{value: 6},{value: 7}];
-  return (
-    <React.Fragment>
-      <List disablePadding>
-        <ListItem>
-          <ListItemText primary="Producto" />
-          <Typography variant="subtitle1">
-            Nombre producto
-          </Typography>
-        </ListItem>
-        <ListItem>
-          <ListItemText primary="Precio" />
-          <Typography variant="subtitle1">
-            $5000
-          </Typography>
-        </ListItem>
-      </List>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="place"
-            name="place"
-            label="Lugar de entrega"
-            fullWidth
-          />
-        </Grid>
+    return (
+      <React.Fragment>
+        <List disablePadding>
+          <ListItem>
+            <ListItemText primary="Producto" />
+            <Typography variant="subtitle1">
+              Nombre producto
+            </Typography>
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="Precio" />
+            <Typography variant="subtitle1">
+              $5000
+            </Typography>
+          </ListItem>
+        </List>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="place"
+              name="place"
+              label="Lugar de entrega"
+              fullWidth
+            />
+          </Grid>
 
-        <Grid item xs={12} sm={6}>
-        <TextField
-          id="standard-select-currency"
-          select
-          label="Dia(s)"
-          value="1"
-        >
-          {numbers.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.value}
-            </MenuItem>
-          ))}
-        </TextField>
+          <Grid item xs={12} sm={6}>
+          <TextField
+            id="standard-select-currency"
+            select
+            label="Dia(s)"
+            value="1"
+          >
+            {numbers.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.value}
+              </MenuItem>
+            ))}
+          </TextField>
+          </Grid>
+          <br/>
+          <br/>
         </Grid>
-        <br/>
-        <br/>
-      </Grid>
-    </React.Fragment>
-  );
+      </React.Fragment>
+    );
+  }
 }
+export default AddressForm;
--- src/Components/rent-product/PaymentForm.js
+++ src/Components/rent-product/PaymentForm.js
@@ -5,50 +5,53 @@ import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 
-export default function PaymentForm() {
-  return (
-    <React.Fragment>
-      <Typography variant="h6" gutterBottom>
-        Método de pago
-      </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required id="cardName"
-            label="Nombre y apellido"
-            fullWidth autoComplete="cc-name" />
+export class PaymentForm extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Typography variant="h6" gutterBottom>
+          Método de pago
+        </Typography>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6}>
+            <TextField
+              required id="cardName"
+              label="Nombre y apellido"
+              fullWidth autoComplete="cc-name" />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              required
+              id="cardNumber"
+              label="Numero de la tarjeta"
+              fullWidth
+              autoComplete="cc-number"
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField 
+              required id="expDate"
+            label="Fecha de expiración"
+            fullWidth autoComplete="cc-exp" />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              required
+              id="cvv"
+              label="CVV"
+              fullWidth
+              autoComplete="cc-csc"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormControlLabel
+              control={<Checkbox color="secondary" name="saveCard" value="yes" />}
+              label="Pagar directamente al vendedor"
+            />
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cardNumber"
-            label="Numero de la tarjeta"
-            fullWidth
-            autoComplete="cc-number"
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField 
-            required id="expDate"
-           label="Fecha de expiración"
-           fullWidth autoComplete="cc-exp" />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cvv"
-            label="CVV"
-            fullWidth
-            autoComplete="cc-csc"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveCard" value="yes" />}
-            label="Pagar directamente al vendedor"
-          />
-        </Grid>
-      </Grid>
-    </React.Fragment>
-  );
+      </React.Fragment>
+    );
+  }
 }
+export default PaymentForm;
