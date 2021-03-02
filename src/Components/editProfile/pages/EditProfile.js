2274253676f815fa1912433a0d80107ce64a2c21
David Andres Vargas Leon <47030575+GEARSHORDA3@users.noreply.github.com>
2021-03-02 17:21:00 -0500

    add component

---
+++ src/Components/editProfile/pages/EditProfile.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Edit from '../Edit';
+
+class EditProfile extends Component {
+ 
+  render() {
+    return (
+      <div>
+        <Edit/>
+      </div>
+    );
+  }
+}
+
+export default EditProfile;
