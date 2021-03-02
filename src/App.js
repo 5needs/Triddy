b4bff9c5abb569be6e1bb390eddfe4cd88afba20
David Andres Vargas Leon <47030575+GEARSHORDA3@users.noreply.github.com>
2021-03-02 10:29:49 -0500

    change name principal folder to myProfile

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import MyPosts from './Components/miPerfil/pages/MyPosts';
-import MyData from './Components/miPerfil/pages/MyData';
+import MyPosts from './Components/myProfile/pages/MyPosts';
+import MyData from './Components/myProfile/pages/MyData';
 
 function App() {
   return (
