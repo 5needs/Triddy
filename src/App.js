dc0dc3fa4b508725d6beea73c7a066bd0f5458ac
David Andres Vargas Leon <47030575+GEARSHORDA3@users.noreply.github.com>
2021-03-02 21:46:19 -0500

    add component header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import MyPosts from './Components/myProfile/pages/MyPosts';
 import MyData from './Components/myProfile/pages/MyData';
+import MenuSlide from './Components/myProfile/MenuSlide';
 
 function App() {
   return (
     <div>
+      <MenuSlide/>
       <div>
         <MyData/>
       </div>
