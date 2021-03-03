8ce9014df7aca55d589a30f27013e85099958187
Alan Marin <alan.marin@mail.escuelaing.edu.co>
2021-03-02 22:10:20 -0500

    Update

---
+++ src/Components/Home/MiniatureProduct.js
@@ -0,0 +1,43 @@
+import { Button, Typography } from '@material-ui/core';
+import React, { Component } from 'react';
+import herramientas from '../Home//images/herramientas.jpg';
+import "./MiniatureProduct.css"
+
+
+class MiniatureProduct extends Component {
+  constructor(props) {
+    super(props);
+    this.image = this.props.image;
+    this.title = this.props.title;
+    this.price = this.props.price;
+    this.state = {};
+  }
+
+
+
+  render() {
+    return (
+      <div className="containerMiniProduct">
+          <div className="miniTittle"> 
+              <Typography variant={this.title.length < 22 ? "h6" : "subtitle2"} align="center">
+                {this.title}
+              </Typography>
+          </div>
+          <div className="miniproduct-containerimage">
+                <img src={herramientas} />
+          </div>
+          <div className="miniProduct-subContainer">
+              <div>
+                  {/* <label>{this.price}</label> */}
+                  <Typography variant="body1" align="justify" classes="proof">Precio: ${this.price}</Typography>
+              </div>
+              <div>
+                  <button> <Typography variant="subtitle1">Rentar</Typography> </button>
+              </div>
+          </div> 
+      </div>
+    );
+  }
+}
+
+export default MiniatureProduct;
