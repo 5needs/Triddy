a6b959e1e916be29cc8187642b926d34e2ee649e
David Andres Vargas Leon <47030575+GEARSHORDA3@users.noreply.github.com>
2021-03-02 23:18:37 -0500

    add componenet header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import './App.css';
 import MyPosts from './Components/viewOtherProfile/pages/MyPosts';
 import MyData from './Components/viewOtherProfile/pages/MyData';
+import MenuSlide from './Components/viewOtherProfile/MenuSlide';
 
 function App() {
   return (
     <div>
       <div>
-        <MyData/>
+        <MenuSlide/>
       </div>
-      <div className="title-list">
-        <h1>Posts</h1>
-      </div>
-      <div>
-        <MyPosts />
+      <div className="viewOtherProfile">
+        <div>
+          <MyData/>
+        </div>
+        <div className="title-list">
+          <h1>Posts</h1>
+        </div>
+        <div>
+          <MyPosts />
+        </div>
       </div>
     </div>
   );
