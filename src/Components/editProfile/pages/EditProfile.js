357f004c269397910be6ede56dff07f20dbc36d2
David Andres Vargas Leon <47030575+GEARSHORDA3@users.noreply.github.com>
2021-03-02 21:28:28 -0500

    add header componenet

--- src/Components/editProfile/pages/EditProfile.js
+++ src/Components/editProfile/pages/EditProfile.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import Edit from '../Edit';
+import MenuSlide from '../MenuSlide';
+
 
 class EditProfile extends Component {
  
   render() {
     return (
       <div>
-        <Edit/>
+          <MenuSlide search={false}/>
+        <div className="Edit-profile">
+          <Edit/>
+        </div>
       </div>
     );
   }
