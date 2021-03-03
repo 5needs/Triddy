91cf2fa3d8602e998a8301e846d4bd567de1ad9a
Edyesid <54111499+Edyesid@users.noreply.github.com>
2021-03-02 20:07:18 -0500

    edit-product

---
+++ src/Components/edit-product/Checkout.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import {ProductEdit} from './ProductEdit';
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    position: 'relative',
+    background: '#fb8c00',
+  },
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+      width: 600,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+    padding: theme.spacing(2),
+    background: '#e0e0e0',
+    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+      marginTop: theme.spacing(6),
+      marginBottom: theme.spacing(6),
+      padding: theme.spacing(3),
+    },
+  },
+  button: {
+    marginTop: theme.spacing(3),
+    marginLeft: theme.spacing(1),
+    background: '#fb8c00',
+  },
+}));
+
+export default function Checkout() {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="absolute" color="primary" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            Triddy
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Typography component="h1" variant="h4" align="center">
+            Editar Producto
+          </Typography>
+          <br/>
+          <React.Fragment>
+            <ProductEdit />
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.button}
+                >
+                Cancelar
+              </Button>
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.button}
+                >
+                Hecho
+              </Button>
+          </React.Fragment>
+        </Paper>
+      </main>
+    </React.Fragment>
+  );
+}
