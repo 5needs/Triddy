8f3be837544e4a435c980886a80d533e5f1593aa
David Andres Vargas Leon <47030575+GEARSHORDA3@users.noreply.github.com>
2021-05-18 21:38:25 -0500

    testing routers to component register

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -3,6 +3,7 @@ import { Switch, Router, Route} from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import { Login } from './login/Login';
 import { PrivateRoute } from './PrivateRoute';
+import RegisterProfile from './registerProfile/pages/Register'
 
 export const history = createBrowserHistory({forceRefresh:true});
 
@@ -19,8 +20,9 @@ export default class Routes extends React.Component {
 		return(
 			<Router history={history}>
 				<Switch>
+					<Route path={"/register_user"} component={RegisterProfile} />	
 					<Route path={"/login"} component={Login} />
-					<PrivateRoute path={"/"} component={this.TestView}/>
+					{/* <PrivateRoute path={"/"} component={this.TestView}/> */}
 				</Switch>
 			</Router>
 		);
